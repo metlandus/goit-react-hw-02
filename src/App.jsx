37e11c1e27a14850dee594d5755a678ed172a362
Mete <metlandus@hotmail.com.tr>
2024-11-25 22:04:32 +0300

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Description from './components/Description'
+import Options from './components/Options'
 
 function App() {
 
+    const feedback = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
 
     return (
         <>
-
+            <Description />
+            <Options />
         </>
     )
 }
---
+++ src/components/Options.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Options = () => {
+    return (
+        <>
+            <button>👍</button>
+            <button>👎</button>
+        </>
+    )
+}
+
+export default Options
