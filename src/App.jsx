d931c23a85c8d8028da1b297ba360607d6111fbe
Mete <metlandus@hotmail.com.tr>
2024-12-09 00:18:51 +0300

    Css including Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import Description from './components/Description'
-import Options from './components/Options'
+import Options from './components/Options/Options'
 import Feedback from "./components/Feedback"
 
 function App() {
-    const [feedback, setFeedback] = useState({
-        good: 0,
-        neutral: 0,
-        bad: 0,
+    const [feedback, setFeedback] = useState(() => {
+        const localFeedback = JSON.parse(localStorage.getItem("feedback"));
+        return (!localFeedback ? {
+            good: 0,
+            neutral: 0,
+            bad: 0
+        } : localFeedback
+        )
     });
+
+    useEffect(() => {
+        localStorage.setItem("feedback", JSON.stringify(feedback));
+    }, [feedback]);
 
     const totalFeedback = feedback.good + feedback.bad + feedback.neutral;
 
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import "./options.css"
 
 const Options = ({ onClick, total }) => {
     return (
-        <>
+        <div className='options'>
             <button onClick={() => onClick("good")}>Good</button>
             <button onClick={() => onClick("neutral")}>Neutral</button>
             <button onClick={() => onClick("bad")}>Bad</button>
             {total > 0 ? <button onClick={() => onClick("reset")}>Reset</button> : ""}
             
-        </>
+        </div>
     )
 }
 
